aed01bbaf9d02602e6416939832818cff58ee7cf
bhaktaravin <ravin.bhakta@gmail.com>
2025-07-21 19:52:10 -0700

    ng build files

---
+++ target/debug/deps/openssl_macros-a66d089eba437a90.d
@@ -0,0 +1,5 @@
+/home/ravin/Code/new_mangadownloader/target/debug/deps/openssl_macros-a66d089eba437a90.d: /home/ravin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/openssl-macros-0.1.1/src/lib.rs
+
+/home/ravin/Code/new_mangadownloader/target/debug/deps/libopenssl_macros-a66d089eba437a90.so: /home/ravin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/openssl-macros-0.1.1/src/lib.rs
+
+/home/ravin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/openssl-macros-0.1.1/src/lib.rs:
